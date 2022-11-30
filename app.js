b23168875743ee320aef0cc2ec158eaa3d78c474
loretari <loretari@gmail.com>
2022-11-30 21:29:53 +0200

    init: add car item

--- app.js
+++ app.js
@@ -13,6 +13,9 @@ const productsDOM = document.querySelector(".products-center")
 // cart
 let cart = []
 
+// buttons
+let buttonsDOM = []
+
 // getting the products
 class Products {
     async getProducts() {
@@ -61,12 +64,48 @@ class UI {
         })
         productsDOM.innerHTML = result
     }
+    getBagButtons() {
+        const buttons = [...document.querySelectorAll(".bag-btn")]
+        // console.log(buttons)
+        buttonsDOM = buttons
+        buttons.forEach((button) => {
+            let id = button.dataset.id
+            // console.log(id)
+            let inCart = cart.find((item) => item.id === id)
+            if (inCart) {
+                button.innerHTML = "In Cart"
+                button.disabled = true
+            }
+            button.addEventListener("click", (event) => {
+                event.target.innerText = "In Cart"
+                event.target.disabled = true
+                // get product from products
+                let cartItem = { ...Storage.getProduct(id), amont: 1 }
+                // console.log(cartItem)
+                // add product to the cart
+                cart = [...cart, cartItem]
+                console.log(cart)
+                // save cart in local storage
+                Storage.saveCart(cart)
+                //  set cart values
+                //  display cart item
+                // show the cart
+            })
+        })
+    }
 }
 
 // local storage
 class Storage {
     static saveProducts(products) {
         localStorage.setItem("products", JSON.stringify(products))
+    }
+    static getProduct(id) {
+        let products = JSON.parse(localStorage.getItem("products"))
+        return products.find((product) => product.id === id)
+    }
+    static saveCart(cart) {
+        localStorage.setItem("cart", JSON.stringify(cart))
     }
 }
 
@@ -76,8 +115,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
     // get all products
     // products.getProducts().then((products) => console.log(products))
-    products.getProducts().then((products) => {
-        ui.displayProducts(products);
-        Storage.saveProducts(products);
-    } )
+    products
+        .getProducts()
+        .then((products) => {
+            ui.displayProducts(products)
+            Storage.saveProducts(products)
+        })
+        .then(() => {
+            ui.getBagButtons()
+        })
 })
